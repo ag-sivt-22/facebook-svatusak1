3db6be8d30048feffe708eb5fdff4b89cf1bd379
Svata <svatuskj@arcig.cz>
2025-02-24 15:23:50 +0100

    create map

--- facebook.py
+++ facebook.py
@@ -1,19 +1,30 @@
+class Facebook:
+    def __init__(self) -> None:
+        self.uzivatele: dict[str, list[str]] = {}
+
+    def pridej_uzivatele(self, *uzivatele: str) -> None:
+        for uzivatel in uzivatele:
+            self.uzivatele[uzivatel] = []
+    def pridej_znamosti(self, *znamosti: tuple[str, str]) -> None:
+        for znamost in znamosti:
+            self.uzivatele[znamost[0]].append(znamost[1])
+            self.uzivatele[znamost[1]].append(znamost[0])
 # Vytvoření instance Facebooku
 fb = Facebook()
 
 # Seznam unikátních jmen
-jmena = [
+
+# Vkládání známostí do Facebooku
+fb.pridej_uzivatele(
     "Adam", "Beata", "Cyril", "Dana", "Emil", "František", "Gabriela", "Hana", "Ivan", "Jana",
     "Karel", "Lenka", "Marek", "Nina", "Ondřej", "Petra", "Quentin", "Radka", "Stanislav", "Tereza",
     "Urbán", "Veronika", "Walter", "Xenie", "Yvona", "Zdeněk", "Alex", "Blanka", "Cecilie", "David"
-]
-
-# Vkládání známostí do Facebooku
-for jmeno in jmena:
-    fb.pridej_uzivatel(jmeno)
+)
   
 # Hardkodované známosti
-znamosti = [
+
+# Vkládání známostí do Facebooku
+fb.pridej_znamosti(
     ("Adam", "Beata"), ("Adam", "Cyril"), ("Beata", "Dana"),
     ("Cyril", "Emil"), ("Cyril", "František"), ("Dana", "Gabriela"),
     ("Emil", "Hana"), ("František", "Ivan"), ("Gabriela", "Jana"),
@@ -24,8 +35,6 @@ znamosti = [
     ("Tereza", "Walter"), ("Urbán", "Xenie"), ("Veronika", "Yvona"),
     ("Walter", "Zdeněk"), ("Xenie", "Alex"), ("Yvona", "Blanka"),
     ("Zdeněk", "Cecilie"), ("Alex", "David"), ("Blanka", "Adam")
-]
+)
 
-# Vkládání známostí do Facebooku
-for clovek1, clovek2 in znamosti:
-    fb.pridej_znamost(clovek1, clovek2)
+print(len(fb.uzivatele["Ondřej"]))
